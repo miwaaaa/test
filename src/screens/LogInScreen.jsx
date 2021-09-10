765b33725ae6d700f7493620a84938434889c35d
miwa <84235823 +miwaaaa@users.noreply.github.com>
2021-09-10 10:20:18 +0900

    Create Loading Component

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -2,12 +2,13 @@ import React, {useState, useEffect} from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert, } from 'react-native';
 import Button from '../components/Button';
 import firebase from 'firebase';
+import Loading from '../components/Loading';
 
 export default function LogInScreen(props) {
     const { navigation } = props;
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+    const [isLoading, setLoading ] = useState(true);
     useEffect(() => {
         const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
             if (user) {
@@ -15,6 +16,8 @@ export default function LogInScreen(props) {
                 index: 0,
                 routes: [{ name: 'MemoList' }],
             });
+            } else {
+                setLoading(false);
             }
         });
         return unsubscribe;
@@ -32,13 +35,16 @@ export default function LogInScreen(props) {
         })
         .catch((error) => {
             Alert.alert(error.code);
-
+        })
+        .then (() => {
+            setLoading(false);
         });
 
     }
 
     return (
         <View style = {styles.container}>
+            <Loading isLoading={isLoading} />
             <View style={styles.inner}>
                 <Text style={styles.title}>Log In</Text>
                 <TextInput
